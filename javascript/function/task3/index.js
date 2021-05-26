63e67f2e3e0f09f043b0d59f182b73bfe2f81e74
Roman Hrynko <romanhrynkoua@gmail.com>
2021-05-26 13:30:57 +0300

    livecoding

--- javascript/function/task3/index.js
+++ javascript/function/task3/index.js
@@ -9,12 +9,38 @@
 // 2. check if number is prime
 // 2.1 Iterate numbers  from 2 to number 10;
 // 2.2 if number  isdivided by any other
+// input numbeer
+// output boolean
+function checkIsPrime(number) {
+  for (let innerNum = 2; innerNum < number; innerNum += 1) {
+    if (number % innerNum === 0) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function getPrimes(num) {
+  for (let number = 2; number < num; number += 1) {
+    console.log('STEP # ' + number);
+
+    if (checkIsPrime(number)) {
+      console.log(number);
+    }
+  }
+}
+console.log(getPrimes(10));
+
 function getPrimes(num) {
   for (let index = 2; index < num; index += 1) {
+    console.log('STEP # ' + index);
     let isPrime = true;
 
     for (let number = 2; number < index; number += 1) {
+      console.log('index is ' + index);
+      console.log('number is ' + number);
       if (index % number === 0) {
+        console.log('number is not prime');
         isPrime = false;
         break;
       }
@@ -25,3 +51,7 @@ function getPrimes(num) {
   }
 }
 console.log(getPrimes(10));
+
+//====================================
+
+const squareArray = arr => (Array.isArray(arr) ? arr.map(num => num * num) : null);
