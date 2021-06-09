f91428e27640f8a67bd5971866744e8afcfd5d86
Roman Hrynko <romanhrynkoua@gmail.com>
2021-06-09 18:47:25 +0300

    coding17

--- javascript/object/task4/index.js
+++ javascript/object/task4/index.js
@@ -1,86 +1,88 @@
-// input: obj, obj;
+// input: obj, obj
 // output: boolean;
-
-// ALGO:
-// 1. check keys length
-// 2. check every key value, if not equal => false;
-
-// option 1
+// algo
+// 1 check keys length
+// 2 check every key value, if not equal => false;
+// 3
+// const compareObjects = (obj1, obj2) =>
+//   Object.keys(obj1).length === Object.keys(obj2).length &&
+//   Object.keys(obj1).every(elem => obj1[elem] === obj2[elem]);
+// -------------------------------------------------------------------
 // function compareObjects(obj1, obj2) {
-//   const arr1 = Object.entries(obj1).join(' ');
-//   const arr2 = Object.entries(obj2).join(' ');
-
-//   return arr1 === arr2;
+//   const keys1 = Object.keys(obj1);
+//   const keys2 = Object.keys(obj2);
+//   if (keys1.length !== keys2.length) return false;
+//   // good
+//   return Object.keys(obj1).every(key => obj1[key] === obj2[key]);
+//   // bad
+//   // return keys1.some((key => obj1[key] !== obj2[key]);
+//   //bad
+//   // return keys1.every((key, index) => key === keys2[index] && obj1[key] === obj2[key]);
 // }
-
-// option 2
+// ----------------------------------------------------------------------
 // function compareObjects(obj1, obj2) {
-//   const key1 = Object.keys(obj1);
-//   const key2 = Object.keys(obj2);
-//   if (key1.length !== key2.length) {
-//     return false;
-//   }
-
-//   return key1.every(key => obj1[key] === obj2[key]);
+//   return Object.keys(obj1).length !== Object.keys(obj2).length
+//     ? false
+//     : Object.keys(obj1).every(key => obj1[key] === obj2[key]);
 // }
+// -----------------------------------------------------------------------
+// good;
+const compareObjects = (obj1, obj2) =>
+  Object.keys(obj1).length === Object.keys(obj2).length &&
+  Object.keys(obj1).every(elem => obj1[elem] === obj2[elem]);
 
-// option 3
-function compareObjects(obj1, obj2) {
-  const key1 = Object.keys(obj1);
-  const key2 = Object.keys(obj2);
-  if (key1.length !== key2.length) {
-    return false;
-  }
-
-  return !key1.some(key => obj1[key] !== obj2[key]);
-}
-
-// option 4
+// --------------------------------------------------------------------
+// bad
 // function compareObjects(obj1, obj2) {
-//   const key1 = Object.keys(obj1);
-//   const key2 = Object.keys(obj2);
-
-//   if (key1.length !== key2.length) {
-//     return false;
-//   }
-
-//   let result = true;
-//   key1.forEach(el => {
-//     if (obj1[el] !== obj2[el]) {
-//       result = false;
+//   //TODO
+//   let length = Object.keys(obj1);
+//   console.log(length);
+//   if (Object.keys(obj1).length !== Object.keys(obj2).length) return false;
+//   // very bad
+//   let obj1Arr = Object.entries(obj1).flat();
+//   let obj2Arr = Object.entries(obj2).flat();
+//   console.log(obj1Arr, obj2Arr);
+//   //very bad
+//   for (let index = 0; index < obj1Arr.length; index += 1) {
+//     // bad
+//     if (obj1Arr[index] !== obj2Arr[index]) {
+//       return false;
 //     }
-//   });
-
-//   return result;
+//   }
+//   return true;
 // }
-
-// option 5
-// function compareObjects(obj1, obj2) {
-//   return JSON.stringify(obj1) === JSON.stringify(obj2);
-// }
-
+// ----------------------------------------------------------------------
+function compareObjects(obj1, obj2) {
+  // very bad
+  const arr1 = Object.keys(obj1).concat(Object.values(obj1));
+  const arr2 = Object.keys(obj2).concat(Object.values(obj2));
+  if (arr1.length !== arr2.length) return false;
+  // very bad
+  for (let i = 0; i < arr1.length; i += 1) {
+    // bad
+    if (arr1[i] !== arr2[i]) return false;
+  }
+  return true;
+}
+// -----------------------------------------------------------------------
 // examples
 const obj1 = {
   name: 'Tom',
   age: 17,
 };
-
 const obj2 = {
   name: 'Bob',
   age: 17,
 };
-
 const obj3 = {
   name: 'Bob',
   age: 17,
   student: false,
 };
-
 const obj4 = {
   name: 'Tom',
   age: 17,
 };
-
 console.log(compareObjects(obj1, obj2)); // ==> false
 console.log(compareObjects(obj2, obj3)); // ==> false
 console.log(compareObjects(obj1, obj4)); // ==> true
